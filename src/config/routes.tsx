53fc5635c5afee23b3695a99c1ca63a71e9f7379
Namjoo-Kim <skawn8rla@gmail.com>
2022-08-08 03:12:32 +0900

    minor

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -5,8 +5,8 @@ import Found404 from '../pages/Found404';
 
 function App() {
     return (
-      // <BrowserRouter>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
+      {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
         <Routes>
           <Route path='/' element={<Welcome />} />
           <Route path='/Home' element={<Home />} />
